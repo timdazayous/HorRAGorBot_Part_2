4e81ea36cc7e0b9e92f5015111ed7e17c4db25ee
timdazayous <tim.dazayous@gmail.com>
2026-06-05 08:28:36 +0200

    fix: uvicorn app string main_api:app

--- main_api.py
+++ main_api.py
@@ -329,7 +329,7 @@ if __name__ == "__main__":
     import uvicorn
 
     uvicorn.run(
-        "main:app",
+        "main_api:app",
         host="0.0.0.0",
         port=8000,
         reload=True,
